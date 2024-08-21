3ec4a6c0b9f80bf33dbda192085b716cf0f0f5dd
Abukhalid <110600419+denjr100@users.noreply.github.com>
2024-08-21 04:45:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+// This bot is free made by :k.xj (Abukhalid)
+// discord : https://discord.gg/2N2WqpUtEy
+//__    __     _____         ______  ________   ______   _______   ________ 
+//|  \  |  \   |     \       /      \|        \ /      \ |       \ |        \
+//| $$\ | $$    \$$$$$      |  $$$$$$\\$$$$$$$$|  $$$$$$\| $$$$$$$\| $$$$$$$$
+//| $$$\| $$      | $$      | $$___\$$  | $$   | $$  | $$| $$__| $$| $$__    
+//| $$$$\ $$ __   | $$       \$$    \   | $$   | $$  | $$| $$    $$| $$  \   
+//| $$\$$ $$|  \  | $$       _\$$$$$$\  | $$   | $$  | $$| $$$$$$$\| $$$$$   
+//| $$ \$$$$| $$__| $$      |  \__| $$  | $$   | $$__/ $$| $$  | $$| $$_____ 
+//| $$  \$$$ \$$    $$       \$$    $$  | $$    \$$    $$| $$  | $$| $$     \
+ //\$$   \$$  \$$$$$$         \$$$$$$    \$$     \$$$$$$  \$$   \$$ \$$$$$$$$
+                                                                           
+   
+const { Client, GatewayIntentBits, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const _0x1caa04=_0x1756;(function(_0x2f63b8,_0x384ecd){const _0x5740fd=_0x1756,_0x489928=_0x2f63b8();while(!![]){try{const _0x1c765c=-parseInt(_0x5740fd(0x13e))/0x1*(-parseInt(_0x5740fd(0x140))/0x2)+-parseInt(_0x5740fd(0x136))/0x3+parseInt(_0x5740fd(0x145))/0x4+parseInt(_0x5740fd(0x133))/0x5+-parseInt(_0x5740fd(0x149))/0x6+parseInt(_0x5740fd(0x13d))/0x7+-parseInt(_0x5740fd(0x135))/0x8;if(_0x1c765c===_0x384ecd)break;else _0x489928['push'](_0x489928['shift']());}catch(_0x256928){_0x489928['push'](_0x489928['shift']());}}}(_0x2dc7,0x6906d));function coloredLog(_0x2409c1,_0x9964b){const _0x418e68=_0x1756;console[_0x418e68(0x146)]('\x1b['+_0x9964b+'m'+_0x2409c1+'\x1b[0m');}client[_0x1caa04(0x144)]('ready',async()=>{const _0x2cdc9c=_0x1caa04;coloredLog(_0x2cdc9c(0x148)+client[_0x2cdc9c(0x142)][_0x2cdc9c(0x139)],_0x2cdc9c(0x13c)),coloredLog(_0x2cdc9c(0x13b),_0x2cdc9c(0x13a)),coloredLog(_0x2cdc9c(0x138),_0x2cdc9c(0x13a)),coloredLog(_0x2cdc9c(0x147),_0x2cdc9c(0x134)),coloredLog('\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20_\x20_\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20_\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20__\x20\x20\x20\x20\x20\x20_\x20\x20_\x20\x0a\x20\x20\x20\x20\x20\x20__|\x20(_)___\x20\x20___\x20___\x20\x20_\x20__\x20__|\x20|\x20\x20__\x20_\x20\x20__\x20_\x20\x20\x20/\x20/\x20__\x20\x20(_)(_)\x0a\x20\x20\x20\x20\x20/\x20_`\x20|\x20/\x20__|/\x20__/\x20_\x20\x5c|\x20\x27__/\x20_`\x20|\x20/\x20_`\x20|/\x20_`\x20|\x20/\x20/\x20\x27_\x20\x5c\x20|\x20||\x20|\x0a\x20\x20\x20\x20|\x20(_|\x20|\x20\x5c__\x20\x5c\x20(_|\x20(_)\x20|\x20|\x20|\x20(_|\x20||\x20(_|\x20|\x20(_|\x20|/\x20/|\x20|\x20|\x20|\x20||\x20||\x20|\x0a\x20\x20\x20\x20\x20\x5c__,_|_|___/\x5c___\x5c___/|_|\x20\x20\x5c__,_(_)__,\x20|\x5c__,\x20/_/\x20|_|\x20|_|/\x20|/\x20|\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20|___/\x20|___/\x20\x20\x20\x20\x20\x20\x20\x20\x20|__/__/\x20\x0a\x20\x20\x20\x20',_0x2cdc9c(0x141));const _0x25cfc8=new SlashCommandBuilder()['setName'](_0x2cdc9c(0x132))[_0x2cdc9c(0x137)](_0x2cdc9c(0x13f));await client['application'][_0x2cdc9c(0x143)]['create'](_0x25cfc8);});function _0x1756(_0x35ab21,_0x1bfb20){const _0x2dc7f6=_0x2dc7();return _0x1756=function(_0x175610,_0x45ac4c){_0x175610=_0x175610-0x132;let _0x1aa482=_0x2dc7f6[_0x175610];return _0x1aa482;},_0x1756(_0x35ab21,_0x1bfb20);}function _0x2dc7(){const _0x54ba3e=['أفرز\x20اعلى\x20اشخاص\x20مسوين\x20انفايت','161194nFQPaM','1;35','user','commands','once','2391024NwFTZc','log','Code\x20by\x20NJ\x20-\x20ﺪﻟﺎﺧ\x20ﻮﺑﺍ','ﻢﺳﺄﺑ\x20ﻞﻴﺠﺴﺗ\x20ﻢﺗ\x20:\x20','3926688fdvSOR','فرز','915870EWwqmA','1;34','6257056EjBJrM','1382538cvfbJe','setDescription','discord.gg/njj','tag','1;91','لﺎﻐﺷ\x20ﻦﻳﻻ\x20نﻭﺍ\x20-\x20ﻲﻔﻄﻳﺎﻣ\x20نﺎﺸﻋ\x20ءﻲﺷ\x20لﺪﻌﺗ\x20وﺍ\x20ﻢﺳﺍ\x20ﺮﻴﻐﺗﻻ','1;93','5749184aeQPdy','9LmQoAs'];_0x2dc7=function(){return _0x54ba3e;};return _0x2dc7();}
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+// discord : https://discord.gg/2N2WqpUtEy   
+
+    if (interaction.commandName === 'فرز') {
+        await interaction.deferReply({ ephemeral: true }); // discord : https://discord.gg/2N2WqpUtEy   
+        var _0x40823d=_0x1214;(function(_0x2aec1e,_0x8ed0b6){var _0x35b40c=_0x1214,_0x7dc71e=_0x2aec1e();while(!![]){try{var _0x2ecab6=-parseInt(_0x35b40c(0x16f))/0x1+parseInt(_0x35b40c(0x172))/0x2*(parseInt(_0x35b40c(0x16b))/0x3)+parseInt(_0x35b40c(0x16c))/0x4+parseInt(_0x35b40c(0x167))/0x5*(parseInt(_0x35b40c(0x170))/0x6)+-parseInt(_0x35b40c(0x16d))/0x7+parseInt(_0x35b40c(0x16a))/0x8+-parseInt(_0x35b40c(0x168))/0x9*(parseInt(_0x35b40c(0x169))/0xa);if(_0x2ecab6===_0x8ed0b6)break;else _0x7dc71e['push'](_0x7dc71e['shift']());}catch(_0x1e7c09){_0x7dc71e['push'](_0x7dc71e['shift']());}}}(_0x5bec,0x83f43),await interaction[_0x40823d(0x16e)]({'content':_0x40823d(0x171)}));function _0x1214(_0x44c920,_0x2fe45b){var _0x5beca4=_0x5bec();return _0x1214=function(_0x121439,_0x254d18){_0x121439=_0x121439-0x167;var _0xd81b0b=_0x5beca4[_0x121439];return _0xd81b0b;},_0x1214(_0x44c920,_0x2fe45b);}function _0x5bec(){var _0x49e99e=['5715787UwEaWV','editReply','21974isnVOH','2058xVcTtZ','**تم\x20إنشاء\x20هذا\x20بوت\x20من\x20قبل\x20\x20في\x20حال\x20تبغى\x20بوتات\x20اكثر\x20حياك\x20سيرفرنا<@707813418924769281>\x0ahttps://discord.gg/2N2WqpUtEy**','9530NmOxDs','14805NrsNBR','7933176sMxHAV','10oNLXil','58168GdDJWK','483UBwLkK','1881612BuQajH'];_0x5bec=function(){return _0x49e99e;};return _0x5bec();}
+        let message; 
+
+        const updateEmbed = async () => {
+            const invites = await interaction.guild.invites.fetch();
+            const inviteCount = {};
+
+            invites.forEach(invite => {
+                const inviterId = invite.inviter.id;
+                if (inviteCount[inviterId]) {
+                    inviteCount[inviterId].uses += invite.uses;
+                } else {
+                    inviteCount[inviterId] = { id: inviterId, code: invite.code, uses: invite.uses };
+                }
+            });
+
+            const sortedInvites = Object.values(inviteCount).sort((a, b) => b.uses - a.uses).slice(0, 10);
+// discord : https://discord.gg/2N2WqpUtEy   
+            const embed = new EmbedBuilder()
+                .setTitle(`أعلى 10 أشخاص مسوين إنفايت في ${interaction.guild.name}`)
+                .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
+                .setColor('#aa1515'); // لون الامبيد
+
+            sortedInvites.forEach((invite, index) => {
+                embed.addFields({
+                    name: `#${index + 1} <@${invite.id}>`, 
+                    value: `**الإنفايت: https://discord.gg/${invite.code}\nعدد الأشخاص: ${invite.uses}\n رصد اعلى انفايت (ONR Abukhalid)**`,
+                    inline: false
+                });
+            });
+
+            const nextUpdate = Math.floor(Date.now() / 1000) + 15; // 15s
+// discord : https://discord.gg/2N2WqpUtEy   
+
+            if (message) {
+                await message.edit({ content: `**الرصد التالي **<t:${nextUpdate}:R>\n@here\n`, embeds: [embed] });
+            } else {
+                message = await interaction.channel.send({ content: `**الرصد التالي **<t:${nextUpdate}:R>\n@here\n`, embeds: [embed] }); // discord : https://discord.gg/2N2WqpUtEy   
+            }
+        };
+
+        
+        await updateEmbed();
+
+        const interval = setInterval(updateEmbed, 15000);
+
+        setTimeout(() => clearInterval(interval), 300000);
+    }
+});
+
+client.login(''); // توكن بوتك
+
+// This bot is free made by :k.xj (Abukhalid)
+// discord : https://discord.gg/2N2WqpUtEy
+//__    __     _____         ______  ________   ______   _______   ________ 
+//|  \  |  \   |     \       /      \|        \ /      \ |       \ |        \
+//| $$\ | $$    \$$$$$      |  $$$$$$\\$$$$$$$$|  $$$$$$\| $$$$$$$\| $$$$$$$$
+//| $$$\| $$      | $$      | $$___\$$  | $$   | $$  | $$| $$__| $$| $$__    
+//| $$$$\ $$ __   | $$       \$$    \   | $$   | $$  | $$| $$    $$| $$  \   
+//| $$\$$ $$|  \  | $$       _\$$$$$$\  | $$   | $$  | $$| $$$$$$$\| $$$$$   
+//| $$ \$$$$| $$__| $$      |  \__| $$  | $$   | $$__/ $$| $$  | $$| $$_____ 
+//| $$  \$$$ \$$    $$       \$$    $$  | $$    \$$    $$| $$  | $$| $$     \
+ //\$$   \$$  \$$$$$$         \$$$$$$    \$$     \$$$$$$  \$$   \$$ \$$$$$$$$
+                                                                           
+   
